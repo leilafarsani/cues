59fc41a4539e361198e8a1c3693f81287058526a
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-05-17 17:16:34 +0100

    Provider Set Up

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "@styles/globals.css";
-import Nav from "@components/Nav";
+import Nav from "@components/Nav"; 
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Cues",
@@ -10,6 +11,7 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
+        <Provider/>
         <div className="main">
           <div className="gradient" />
         </div>
